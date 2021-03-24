afea078167b6467c3630340c708f0f23e1ccd685
grtlinux <grtlinux@gmail.com>
2021-03-24 18:04:05 +0900

    create 1st

---
+++ ver00.00/KieaMonster-04-websocket-03/src/main/java/org/tain/KieaMonster04Websocket03Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster04Websocket03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster04Websocket03Application.class, args);
+	}
+
+}
---
+++ ver00.00/KieaMonster-04-websocket-03/src/test/java/org/tain/KieaMonster04Websocket03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaMonster04Websocket03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
