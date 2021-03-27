463fb3c8b1bf92ccad220b53fff56dfeebd9dfd5
grtlinux <grtlinux@gmail.com>
2021-03-28 00:29:19 +0900

    ...

--- ver00.00/KieaMonster-04-websocket-03/src/main/java/org/tain/config/WebSocketConfig.java
+++ ver00.00/KieaMonster-04-websocket-03/src/main/java/org/tain/config/WebSocketConfig.java
@@ -14,14 +14,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws").withSockJS();
 	}
-	
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("/app");
-		registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
-		
-		// Use this for enabling a full featured broker like RabbitMQ
+
+		// Enables a simple in-memory broker
+		registry.enableSimpleBroker("/topic");
+
 		/*
+		// Use this for enabling a full featured broker like RabbitMQ
 		registry.enableStompBrokerRelay("/topic")
 			.setRelayHost("localhost")
 			.setRelayPort(61613)
