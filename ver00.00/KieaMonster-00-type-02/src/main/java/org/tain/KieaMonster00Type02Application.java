b1860a2fe6aaf15d643780122baec01ee8516d39
grtlinux <grtlinux@gmail.com>
2021-04-06 14:15:36 +0900

    create 1st

---
+++ ver00.00/KieaMonster-00-type-02/src/main/java/org/tain/KieaMonster00Type02Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster00Type02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster00Type02Application.class, args);
+	}
+
+}
