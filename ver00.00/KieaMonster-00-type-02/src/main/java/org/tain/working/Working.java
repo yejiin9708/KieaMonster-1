747e5724b685c2089125c9e5dd19a6ce1b4d22d8
grtlinux <grtlinux@gmail.com>
2021-04-12 13:36:42 +0900

    ...

--- ver00.00/KieaMonster-00-type-02/src/main/java/org/tain/working/Working.java
+++ ver00.00/KieaMonster-00-type-02/src/main/java/org/tain/working/Working.java
@@ -2,7 +2,6 @@ package org.tain.working;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tain.properties.ProjEnvBaseProperties;
 import org.tain.utils.CurrentInfo;
 import org.tain.utils.Flag;
 import org.tain.working.properties.PropertiesWorking;
@@ -13,16 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Working {
 
-	@Autowired
-	private ProjEnvBaseProperties projEnvBaseProperties;
-	
 	public void work() throws Exception {
 		log.info("KANG-20210405 >>>>> {} {}", CurrentInfo.get());
 		
 		if (Flag.flag) jobForProperties();
-		
-		// whether to exit
-		if (this.projEnvBaseProperties.isTestFlag()) System.exit(0);
 	}
 	
 	///////////////////////////////////////////////////////////////////////////
