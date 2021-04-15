126385a626f26a092c0efffaaa293ccc6f37cc06
grtlinux <grtlinux@gmail.com>
2021-04-15 17:51:01 +0900

    make conversation to hide

--- ver00.00/KieaMonster-04-websocket-02/src/main/resources/static/app.js
+++ ver00.00/KieaMonster-04-websocket-02/src/main/resources/static/app.js
@@ -44,8 +44,9 @@ $(function () {
 	$("form").on('submit', function (e) {
 		e.preventDefault();
 	});
-	$( "#connect" ).click(function() { connect(); });
-	$( "#disconnect" ).click(function() { disconnect(); });
-	$( "#send" ).click(function() { sendName(); });
+	$("#connect").click(function() { connect(); });
+	$("#disconnect").click(function() { disconnect(); });
+	$("#send").click(function() { sendName(); });
+	$("#conversation").hide();
 });
 
