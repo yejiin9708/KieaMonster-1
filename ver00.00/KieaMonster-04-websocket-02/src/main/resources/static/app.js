906bc8591e32098ef0cf37ca21be605a6e928745
grtlinux <grtlinux@gmail.com>
2021-04-15 15:36:29 +0900

    ...

--- ver00.00/KieaMonster-04-websocket-02/src/main/resources/static/app.js
+++ ver00.00/KieaMonster-04-websocket-02/src/main/resources/static/app.js
@@ -1,6 +1,6 @@
 var stompClient = null;
 
-function setConnected(connected) {
+function _setConnected(connected) {
 	$("#connect").prop("disabled", connected);
 	$("#disconnect").prop("disabled", !connected);
 	if (connected) {
@@ -12,14 +12,18 @@ function setConnected(connected) {
 	$("#greetings").html("");
 }
 
+function _showGreeting(message) {
+	$("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
 function connect() {
 	var socket = new SockJS('/v0.2/gs-guide-websocket');
 	stompClient = Stomp.over(socket);
 	stompClient.connect({}, function (frame) {
-		setConnected(true);
+		_setConnected(true);
 		console.log('Connected: ' + frame);
 		stompClient.subscribe('/topic/greetings', function (greeting) {
-			showGreeting(JSON.parse(greeting.body).content);
+			_showGreeting(JSON.parse(greeting.body).content);
 		});
 	});
 }
@@ -28,16 +32,12 @@ function disconnect() {
 	if (stompClient !== null) {
 		stompClient.disconnect();
 	}
-	setConnected(false);
+	_setConnected(false);
 	console.log("Disconnected");
 }
 
 function sendName() {
 	stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
-}
-
-function showGreeting(message) {
-	$("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
 
 // $(document).ready(...)
