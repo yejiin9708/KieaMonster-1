4d279c6169c73a3376856857698bf3a2cad2d558
grtlinux <grtlinux@gmail.com>
2021-04-20 13:23:43 +0900

    springboot jsp

--- ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/controller/MainController.java
+++ ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/controller/MainController.java
@@ -1,12 +1,31 @@
 package org.tain.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
+	@RequestMapping("/1")
+	public String index1() {
+		return "index";
+	}
+	
+	@RequestMapping("/2")
+	public String index2(Model model) {
+		model.addAttribute("name", "Kiea Seok");
+		return "index";
+	}
+	
+	@RequestMapping("/3")
+	public String index3(ModelMap mapModel) {
+		mapModel.addAttribute("name", "Kiea Seok");
+		return "index";
+	}
+	
 	@RequestMapping("/")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("index2");
@@ -14,4 +33,5 @@ public class MainController {
 		mav.addObject("name", "Kiea");
 		return mav;
 	}
+	
 }
