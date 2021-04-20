edda06a23c6cfd9b4a6c229f77a5e8124622ef1a
grtlinux <grtlinux@gmail.com>
2021-04-20 14:22:03 +0900

    ...

--- ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/controller/MainController.java
+++ ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/controller/MainController.java
@@ -6,6 +6,27 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/*
+ * Model, ModelMap 공통점
+ * 
+ *     model.addAttribute("변수명");
+ *     modelMap.addAttribute("변수명");
+ *     둘 다 addAttribute를 사용함
+ *     Model or ModelMap에 데이터만 저장 후 View에서 사용목적
+ *  
+ * 
+ * Model, ModelMap 차이점
+ * 
+ *     Model - 인터페이스
+ *     ModelMap - 클래스
+ * 
+ * ModelAndView
+ * 
+ *     addObject를 통해 데이터를 저장
+ *     setViewName을 통해 이동하고자 하는 View를 저장
+ *     메소드 안에서 ModelAndView mv = new ModelAndView(); 
+ *     return type ModelAndView 
+ */
 @Controller
 public class MainController {
 
