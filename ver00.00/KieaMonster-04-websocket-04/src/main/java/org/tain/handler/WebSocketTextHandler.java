fde7c7f3a41c0f48e91cde16ceba56df143b5d29
grtlinux <grtlinux@gmail.com>
2021-04-20 13:49:53 +0900

    .....

--- ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/handler/WebSocketTextHandler.java
+++ ver00.00/KieaMonster-04-websocket-04/src/main/java/org/tain/handler/WebSocketTextHandler.java
@@ -2,6 +2,7 @@ package org.tain.handler;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -13,7 +14,14 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class WebSocketTextHandler extends TextWebSocketHandler {
 
 	// ArrayList<WebSocketSession> sessions = new ArrayList<>();
-	HashMap<String, WebSocketSession> sessions = new HashMap<>();
+	private Map<String, WebSocketSession> sessions = new HashMap<>();
+
+	// 세션이 생성될때 시작되는 함수
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		super.afterConnectionEstablished(session);
+		this.sessions.put(session.getId(), session);
+	}
 
 	// client에서 메시지가 서버로 전송댈때 실행되는 함수.
 	@Override
@@ -30,13 +38,6 @@ public class WebSocketTextHandler extends TextWebSocketHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	// 세션이 생성될때 시작되는 함수
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		super.afterConnectionEstablished(session);
-		this.sessions.put(session.getId(), session);
 	}
 
 	// 세션이 끝날때 실행되는 함수
