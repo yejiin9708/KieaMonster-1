c3506fd0ad648a949cb01350f71dc0e1004c3bd7
grtlinux <grtlinux@gmail.com>
2021-04-27 18:26:26 +0900

    on doing

--- ver00.00/KieaMonster-11-worker-01/src/main/java/org/tain/config/AsyncConfig.java
+++ ver00.00/KieaMonster-11-worker-01/src/main/java/org/tain/config/AsyncConfig.java
@@ -3,10 +3,7 @@ package org.tain.config;
 import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.tain.properties.ProjEnvBaseProperties;
 import org.tain.utils.CurrentInfo;
