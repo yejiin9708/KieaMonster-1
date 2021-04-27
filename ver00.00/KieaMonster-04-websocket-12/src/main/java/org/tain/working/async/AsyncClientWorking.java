7aec64935186ed8e61971e6246dff0ebc1a2f5d8
grtlinux <grtlinux@gmail.com>
2021-04-27 15:45:59 +0900

    packet test by 1MB

--- ver00.00/KieaMonster-04-websocket-12/src/main/java/org/tain/working/async/AsyncClientWorking.java
+++ ver00.00/KieaMonster-04-websocket-12/src/main/java/org/tain/working/async/AsyncClientWorking.java
@@ -27,11 +27,30 @@ public class AsyncClientWorking {
 	public void test01() throws Exception {
 		log.info("KANG-20210405 >>>>> {} {}", CurrentInfo.get());
 		
-		if (Flag.flag) {
+		if (!Flag.flag) {
 			for (int i=0; ; i++) {
 				String message = String.format("message number = %04d", i);
 				this.taskSimpleClient.sendMessage(message);
 				Sleep.run(10 * 1000);
+			}
+		}
+		
+		if (Flag.flag) {
+			StringBuffer sb = new StringBuffer();
+			// 10    -> 1k
+			// 100   -> 10k
+			// 1000  -> 100k
+			// 10000 -> 1M
+			for (int i=0; i < 10000; i++) {
+				sb.append("12345678901234567890123456789012345678901234567890");
+				sb.append("12345678901234567890123456789012345678901234567890");
+				sb.append("\n");
+			}
+			
+			for (int i=0; ; i++) {
+				String message = String.format("%s [%04d]", sb.toString(), i);
+				this.taskSimpleClient.sendMessage(message);
+				Sleep.run(20 * 1000);
 			}
 		}
 	}
