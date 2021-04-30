a8a6a7e08ed6645bc3d4ec215c38c9eeef74c75d
grtlinux <grtlinux@gmail.com>
2021-04-30 13:58:02 +0900

    create 1st

---
+++ ver00.00/KieaMonster-93-wspage-93/src/main/java/org/tain/KieaMonster93Wspage93Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster93Wspage93Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster93Wspage93Application.class, args);
+	}
+
+}
