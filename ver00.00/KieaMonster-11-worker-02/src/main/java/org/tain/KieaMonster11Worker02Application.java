9eefaa86620d1979abd3b11ce6ab825d038d9a63
grtlinux <grtlinux@gmail.com>
2021-05-05 12:10:22 +0900

    create 1st

---
+++ ver00.00/KieaMonster-11-worker-02/src/main/java/org/tain/KieaMonster11Worker02Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster11Worker02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster11Worker02Application.class, args);
+	}
+
+}
