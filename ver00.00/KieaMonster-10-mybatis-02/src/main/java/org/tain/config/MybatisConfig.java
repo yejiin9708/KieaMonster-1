8d11af3de8ff0d1874ea0929874de4d1055e9cef
grtlinux <grtlinux@gmail.com>
2021-05-26 07:26:36 +0900

    create 1st

---
+++ ver00.00/KieaMonster-10-mybatis-02/src/main/java/org/tain/config/MybatisConfig.java
@@ -0,0 +1,34 @@
+package org.tain.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"org.tain.mappers"})
+public class MybatisConfig {
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		final SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mappers/**/*Mapper.xml"));
+		sqlSessionFactoryBean.setConfigLocation(resolver.getResource("classpath:mappers/mybatis_config.xml"));
+		sqlSessionFactoryBean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
+		return sqlSessionFactoryBean.getObject();
+	}
+}
---
+++ ver00.00/KieaMonster-10-mybatis-02/src/test/java/org/tain/KieaMonster10Mybatis02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaMonster10Mybatis02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
