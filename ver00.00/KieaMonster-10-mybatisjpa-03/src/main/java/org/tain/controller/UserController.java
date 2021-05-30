18af3d53839c819403af876f1048e2cfce302032
grtlinux <grtlinux@gmail.com>
2021-05-30 14:57:18 +0900

    .....

--- ver00.00/KieaMonster-10-mybatisjpa-03/src/main/java/org/tain/controller/UserController.java
+++ ver00.00/KieaMonster-10-mybatisjpa-03/src/main/java/org/tain/controller/UserController.java
@@ -2,11 +2,15 @@ package org.tain.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.tain.jpa.domain.TbUser;
 import org.tain.jpa.repository.TbUserRepository;
 import org.tain.mybatis.mappers.UserMapper;
@@ -26,6 +30,13 @@ public class UserController {
 	
 	@GetMapping(value = {"/{id}"})
 	public List<User> user(@PathVariable("id") Long id) {
+		if (Boolean.TRUE) {
+			HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+			String ip = request.getHeader("X-FORWARDED-FOR");
+			if (ip == null)
+				ip = request.getRemoteAddr();
+			System.out.println(">>>>> Client IP: " + ip);
+		}
 		return this.userMapper.selectAllUsers(id);
 	}
 	
@@ -38,6 +49,13 @@ public class UserController {
 	
 	@GetMapping(value = {"/list2"})
 	public List<TbUser> list2() {
+		if (Boolean.TRUE) {
+			HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+			String ip = request.getHeader("X-FORWARDED-FOR");
+			if (ip == null)
+				ip = request.getRemoteAddr();
+			System.out.println(">>>>> Client IP: " + ip);
+		}
 		return this.tbUserRepository.findAll();
 	}
 }
